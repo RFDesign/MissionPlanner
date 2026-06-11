ac44d996def2a25de267ce8d41f2983c8aa7a8b0
StevenHAtRFDesign <stevenh@rfdesign.com.au>
2026-06-11 17:33:23 +1000

    2.69 - Fixed issue in installer.

--- SikRadio/Properties/AssemblyInfo.cs
+++ SikRadio/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.68.0")]
-[assembly: AssemblyFileVersion("0.2.68.0")]
+[assembly: AssemblyVersion("0.2.69.0")]
+[assembly: AssemblyFileVersion("0.2.69.0")]
